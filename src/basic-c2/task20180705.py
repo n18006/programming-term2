a39aae54077b86b47231c6d2f77fb0d246cbc8df
=n18006 <=n18006@std.it-college.ac.jp>
2018-07-10 14:43:58 +0900

    0705課題の修正

--- src/basic-c2/task20180705.py
+++ src/basic-c2/task20180705.py
@@ -1,17 +1,24 @@
-# ビールの合計
-bear = 100 * 2
+# 値段 
+beer_v = 200
+otumami_v = 100
+yakitori_v = 100
 
-# おつまみの合計
-otumami = 100 * 1
+# 個数
+beer_c = 2
+otumami_c = 1
+yakitori_c = 2
 
-# 焼鳥の合計　※セールで２割引
-yakitori = (100 * 0.2) * 2
+# 割引率
+yakitori_sale = 0.2
 
-# ポイントカード
-point_card  = 150
+# ポイントカード 
+point = 150
 
-# 総合計
-sum_t = bear + otumami + yakitori
+# 計算
+beer_sum = beer_v * beer_c
+otumami_sum = otumami_v * otumami_c
+yakitori_sum = yakitori_v * (1 - yakitori_sale) * yakitori_c
+pay = beer_sum + otumami_sum + yakitori_sum - point
 
 # お会計
-print('お支払は',sum_t - point_card,'円です。')
+print('お支払は', pay,'円です。')

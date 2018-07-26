56971ba38b27429d15bb6461ac2aa82c9b6ef71a
=n18006 <=n18006@std.it-college.ac.jp>
2018-07-26 10:43:12 +0900

    0718の課題提出

--- src/basic-c3/task20180718.py
+++ src/basic-c3/task20180718.py
@@ -17,28 +17,35 @@ option_menu = {'LowFatMilk': 0,
                'ChocoSource': 0,
                'DeCafe': 50}
 
-# 注文した内容@メインメニュー
+# 注文した内容
 order_list = []
 order_price = []
-
+# ループを抜けるスイッチ
+cto = True #cod >> Continue to order の略
 # 処理開始
-while True:
-    user = input('メインメニューを選んでください>>>')
-    if user == "" or user == "q" or user == "Q":
-        print('キャンセルされました。またのご利用お待ちしております。')
+while cto: #main_menuの注文
+    input_val = input("メインメニューを選んでください>>>")
+    if input_val == "" or input_val == "q":
+        print("注文をキャンセルいたします")
         break
-
-    elif user not in main_menu:
-        print('選択されたメニューはありません。')
+    elif input_val not in main_menu:
+        print("選択されたメニューはありません")
         continue
-    else:
-        order_list.append(user)
-        order_price.append(main_menu[user])
-    break
-
-# 注文内容の結果表示
-print('以下の内容で注文を承りました。')
-print(order_list)
-print(order_price)
-print('右奥のカウンターにてお待ちくださいませ。')
-#　メインメニューの選択、表示までは動作確認できたので、ひとまず提出します。
+    elif input_val in main_menu:
+        order_list.append(input_val)
+        order_price.append(main_menu[input_val])
+        print("メインメニューを承りました")
+        cto = False
+        input_val = input("オプションメニューを選んでください>>>") #option_menuの注文
+        while True:
+            if input_val in option_menu:
+                order_list.append(input_val)
+                order_price.append(option_menu[input_val])
+                input_val = input("他にオプションメニューの注文はございますか？>>>")
+            elif input_val == "" or input_val == "q":
+                break
+                cto = False
+    # 注文内容の確認
+    print("注文内容は" ,order_list, "です")
+    sum_v = sum(order_price) #合計金額の計算
+    print("合計金額は", sum_v, "円です。右奥のカウンターにてお待ち下さい。")

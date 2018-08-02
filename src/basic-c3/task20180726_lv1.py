9abd8eaf1b2820b38f24eb07678209b618d0f029
=n18006 <=n18006@std.it-college.ac.jp>
2018-08-02 11:35:18 +0900

    0726課題の提出

--- src/basic-c3/task20180726_lv1.py
+++ src/basic-c3/task20180726_lv1.py
@@ -1,18 +1,45 @@
 import random
 
-goal_pos = 10
-cur_x = 0
-dice_value = 0
+goal_pos = 10 #双六のゴール位置
+cur_x = 0 #現在位置.初期化の為０を代入
+dice_value = 0 #サイコロの出た目を代入する変数。初期化の為０を代入。
 
+# 関数の設定
 def shake_dice():
+    '''
+    1~6までのサイコロの目をランダムな数字で出力
+
+    Parameters
+    ----------
+    なし
+
+    Returns
+    -------
+    int : 1~6までのランダムな数値
+
+    '''
     return random.randint(1, 6)
 
 
 def go_forward(n):
+    '''
+    進んだマスの合計値
+
+    Parameters
+    ----------
+    n : int
+    進む数値
+
+    Returns
+    -------
+    なし
+
+    '''
+
     global cur_x
     cur_x += n
 
-
+# メイン処理
 while cur_x < goal_pos:
     input("サイコロを振ってください>>>")
     dice_value = shake_dice()
